b6d3d23200c07d800e8f4f1856b31fc50eb8d36a
José Felix <jf-junior9@hotmail.com.br>
2024-07-10 21:02:39 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,43 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java ");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJose = new Dev();
+        devJose.setNome("José Felix");
+        devJose.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos José Felix " + devJose.getConteudosInscritos());
+
+        devJose.progredir();
+        devJose.progredir();
+        devJose.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos José Felix " + devJose.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos José Felix " + devJose.getConteudosInscritos());
+        System.out.println("XP: " + devJose.calcularTotalXP());
+
+
+        System.out.println("------------------");
+
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Maria " + devMaria.getConteudosInscritos());
+
+        devMaria.progredir();
+        System.out.println("-");
+
+
+        System.out.println("Conteudos Concluidos Maria " + devMaria.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Maria " + devMaria.getConteudosInscritos());
+        System.out.println("XP: " + devMaria.calcularTotalXP());
+
+
     }
 }
